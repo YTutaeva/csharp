9fbb7902aeb21bc8bca0772e48dd4ed49c10aeca
Yulia <7637979@gmail.com>
2023-02-05 22:48:33 +0300

    Homework Seminar 3

---
+++ csharp_seminar3/task19_hw/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+
+Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+while (number >= 9999 || number < 99999)
+{
+    Console.Write("Введите пятизначное число: ");
+    int number = int.Parse(Console.Readline()!);
+}
+
+if (number > 9999 || number <= 99999)
+{
+num1 = number * 100;
+num2 = number % 10 + number % 100;
+
+    if (num1 == num 2)
+    {
+    Console.WriteLine("да");
+    }
+    else
+    {
+    Console.WriteLine("нет");
+    }
+}
