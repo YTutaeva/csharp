7e8236881662cb75ff06566494b2f17e4e476d69
Yulia <7637979@gmail.com>
2023-02-07 16:11:30 +0300

    task19,task21,task23

--- csharp_seminar3/task19_hw/Program.cs
+++ csharp_seminar3/task19_hw/Program.cs
@@ -8,24 +8,35 @@ Console.Clear();
 
 Console.Write("Введите пятизначное число: ");
 int number = int.Parse(Console.ReadLine()!);
+int num1 = 0;
+int num2 = 0;
+int num4 = 0;
+int num5 = 0;
 
-while (number >= 9999 || number < 99999)
+if (number >= 100000 || number < 10000)
 {
-    Console.Write("Введите пятизначное число: ");
-    int number = int.Parse(Console.Readline()!);
-}
-
-if (number > 9999 || number <= 99999)
-{
-num1 = number * 100;
-num2 = number % 10 + number % 100;
-
-    if (num1 == num 2)
+    Console.WriteLine("Число не пятизначное, повторите команду и введите пятизначное число ");
+}   
+    
+else if (number > 9999 || number <= 99999)
     {
-    Console.WriteLine("да");
-    }
+        num1 = number / 10000;
+        num2 = number / 1000 % 10;
+        num4 = number % 100 / 10;
+        num5 = number % 10;
+
+    if (num1 == num5 && num2 == num4)
+
+        {
+            Console.WriteLine($"Число {number} является палиндромом");
+        }
+
     else
-    {
-    Console.WriteLine("нет");
+        {
+            Console.WriteLine($"Число {number} не является палиндромом");
+        }
+
     }
-}
+
+
+
--- csharp_seminar3/task21_hw/Program.cs
+++ csharp_seminar3/task21_hw/Program.cs
@@ -1,3 +1,24 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.Write("Введите координаты точки Ax: ");
+int Ax = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки Ay: ");
+double Ay = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координаты точки Az: ");
+double Az = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Введите координаты точки Bx: ");
+int Bx = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки By: ");
+double By = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координаты точки Bz: ");
+double Bz = Convert.ToDouble(Console.ReadLine()!);
+
+double distance = Math.Sqrt(Math.Pow(Ax - Bx, 2) + Math.Pow(Ay - By, 2) + Math.Pow(Az - Bz, 2));
+
+
+Console.WriteLine($"Расстояние между точками А ({Ax}, {Ay}, {Az}) и B ({Bx}, {By}, {Bz}) в 3D пространстве равно {distance:f2}");
