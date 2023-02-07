37a0a5e4a873108bca6f356a37a9303b738e9ad0
Yulia <7637979@gmail.com>
2023-02-07 14:48:07 +0300

    Seminar 4

---
+++ csharp_seminar4/task24/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 24: Напишите программу, которая
+// принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Clear();
+int DigitCount(int A)
+{
+    int count = 0;
+    if (A == 0)
+    {
+        count = 1;
+    }
+    else
+    {
+        while (A != 0)
+        {
+            A /= 10;
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Количество цифр числа {N} равно {DigitCount(N)}");
---
+++ csharp_seminar4/task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.Clear();
+
+int Multi(int number)
+{
+    int prod = 1;
+
+    for (int i = 2; i <= number; i++)
+    {
+        prod *= i; // prod = prod * i
+    }
+    return prod;
+}
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine($"Произведения чисел от 1 до {N} равно {Multi(N)}");
