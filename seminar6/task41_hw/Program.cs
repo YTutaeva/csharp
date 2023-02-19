57b0bceecb12bee51c2c754413a7717f1e74887d
Yulia <7637979@gmail.com>
2023-02-19 22:27:31 +0300

    task 41,43

--- seminar6/task41_hw/Program.cs
+++ seminar6/task41_hw/Program.cs
@@ -1,2 +1,36 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetPositive(int [] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество генерируемых чисел: ");
+int size = int.Parse(Console.ReadLine()!);
+int[] array = GetArray(size, -30, 30);
+
+Console.Write(String.Join(", ", array));
+Console.Write(" -> ");
+Console.Write(String.Join(", ", GetPositive(array)));
+Console.WriteLine();
--- seminar6/task43_hw/Program.cs
+++ seminar6/task43_hw/Program.cs
@@ -2,3 +2,33 @@
 // пересечения двух прямых, заданных уравнениями y = k1 *
 // x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
 // пользователем.
+
+Console.Clear();
+
+double ResultX(int b1, int k1, int b2, int k2)
+{
+    double x1 = b2 - b1;
+    double x2 = k1 - k2;
+    double x = x1 / x2;
+    return x;
+}
+
+double ResultY(int k1, int b1, double x)
+{
+    double y = (k1 * x) + b1;
+    return y;
+}
+
+Console.Write("Введите значение b1: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k1: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение b2: ");
+int b2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение k2: ");
+int k2 = int.Parse(Console.ReadLine()!);
+
+double x = ResultX(b1, k1, b2, k2);
+double y = ResultY(k1, b1, x);
+
+Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> (x; {y}.");
